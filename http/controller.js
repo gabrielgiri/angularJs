f8970ff6ef4dee6a60fd6bb6dbe98e7dbf7f3baa
gabrielgiri <gabrielgiri@gmail.com>
2019-05-14 11:49:59 -0300

    using post

--- http/controller.js
+++ http/controller.js
@@ -1,27 +1,32 @@
 angular.module("MyFirstApp",[])
     .controller("FirstController", function($scope, $http){
         $scope.users = [];
+        $scope.newUser = [];
 
         $http.get("http://jsonplaceholder.typicode.com/users")
 
-            .success(function(data){
-                             
+            .success(function(data){             
                 $scope.users = data;
                 console.log(data);
-
-            })
+                })
+                 
             .error(function(error){
 
             });
 
-            $scope.addPost = function(){
-                $http.post("http://jsonplaceholder.typicode.com/users")
-                .success(function(data, status, headers, config){
-                    
+            $scope.addUser = function(){
+                
+                 $http.post("http://jsonplaceholder.typicode.com/users",{
+                     title: $scope.newUser.name,
+                     title: $scope.newUser.email
                 })
+                 .success(function(data, status, headers, config){
+                    $scope.users.push($scope.newUser);
+                    $scope.addUser={};
+                 })
 
-                .error(function(error, status, headers, config){
-                    console.log(error);
-                };
-            }
+                 .error(function(error, status, headers, config){
+                     console.log(error);
+                 });
+             }
     }); 
