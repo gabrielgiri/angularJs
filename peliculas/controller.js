a5aeffbadabd11ce988f564b2f41a329c7db3daa
gabrielgiri <gabrielgiri@gmail.com>
2019-05-14 10:50:57 -0300

    Final Peliculas

--- peliculas/controller.js
+++ peliculas/controller.js
@@ -1,12 +1,48 @@
-angular.module("MyFirstApp",[])
-    .controller("FirstController", function($scope,$http){
-
-        $http.get("http://jsonplaceholder.typicode.com/post")
-            .success(function(data){
-                console.log(data);
-
-            })
-            .error(function(error){
-
-            })
-    });
+angular.module("peliculas",[])
+    .controller("movies", ["$scope",function(m){
+        m.titulo = "Buscador de Peliculas";
+        m.nombre = "Gabriel";
+        m.nuevaPeli = {};
+        m.peliculas = [
+            {
+                titulo: "No country for old man",
+                fecha: "2008",
+                director: "James Cameron",
+                oscars: "4"
+            },
+            {
+                titulo: "Memento",
+                fecha: "1997",
+                director: "James Cameron",
+                oscars: "0"
+            },
+            {
+                titulo: "Volver al futuro",
+                fecha: "1985",
+                director: "Zemectik",
+                oscars: "4"
+            },
+            {
+                titulo: "Titanic",
+                fecha: "1997",
+                director: "James Cameron",
+                oscars: "4"
+            },
+            {
+                titulo: "El Padrino",
+                fecha: "1972",
+                director: "Ford Coppola",
+                oscars: "4"
+            },
+            {
+                titulo: "El rey leon",
+                fecha: "1994",
+                director: "James Cameron",
+                oscars: "3"
+            }
+        ];
+        m.agregarPelicula = function(){
+            m.peliculas.push(m.nuevaPeli);
+            m.nuevaPeli = {};
+        }
+    }]);
